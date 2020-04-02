0a5d85824be8bc8a78c52c2c856d1af0c418400b
Carranza94 <jgca9417@gmail.com>
2020-04-02 16:30:28 -0500

    desactualizado

--- src/main.js
+++ src/main.js
@@ -3,10 +3,367 @@ import {
   search,
   order,
   filterByType,
+  dpsCalculate,
+  epsCalculate,
 } from './data.js';
 import data from './data/pokemon/pokemon.js';
-//
+
 const sectionContent = document.querySelector('.content');
+
+// MODAL FEATURES
+const featuresCard = (pokemon) => {
+  const getTypes = (arr) => {
+    let types = '';
+    arr.forEach((resist) => {
+      types += `${resist} `;
+    });
+    return types;
+  };
+  // Card
+  const sectionModal = document.createElement('section');
+  sectionModal.classList.add('modal-container', 'modal-close');
+  sectionModal.classList.toggle('modal-close');
+  const cardContainer = document.createElement('div');
+  cardContainer.innerHTML = `
+  <div class="modal">
+    <p class="close green-bg">x</p>
+    <section class="cards">
+      <section class="pokemon-name2 green-bg">${pokemon.num} - ${pokemon.name}</section>
+      <section class="info-container">
+        <div class="sub-container-img">
+          <p class="subtitle2">${pokemon['pokemon-rarity']}</p>
+          <div class="pokemon-screen green-b">
+            <img class="screen-img" src="${pokemon.img}">
+          </div>
+        </div>
+        <div class="sub-container-text">
+          <p class="subtitle2">generation</p>
+          <p class="p-bottom">N° ${pokemon.generation.num.slice(10).toUpperCase()} - ${pokemon.generation.name}</p>
+          <p class="subtitle2">type</p>
+          <p class="p-bottom">${getTypes(pokemon.type)}</p>
+          <p class="subtitle2">size</p>
+          <p >Height: ${pokemon.size.height}</p>
+          <p >Weight: ${pokemon.size.weight}</p>
+        </div>
+      </section>
+      <section class="info-container">
+        <div class="column">
+          <p class="subtitle2 h-stat">Encounter</p>
+          <div class="number-data">
+            <p>Base flee rate</p>
+            <p class="num-cel">${(pokemon.encounter['base-flee-rate'] * 100).toFixed(1)}%</p>
+          </div>
+          <div class="number-data">
+            <p>Base capture rate</p>
+            <p class="num-cel">${(pokemon.encounter['base-capture-rate'] * 100).toFixed(1)}%</p>
+          </div>
+        </div>
+        <div class="column">
+          <p class="subtitle2 h-stat">Spawn chance</p>
+          <p class="num-cel">${(pokemon['spawn-chance'] * 100).toFixed(2)}%</p>
+        </div>
+      </section>
+
+      <section class="info-container">
+        <p class="subtitle2 t-evol">Evolutions</p>
+        <div class="evolutions"></div>
+      </section>
+    </section>
+  </div>
+  `;
+
+  const close = cardContainer.querySelector('.close');
+  close.addEventListener('click', () => {
+    sectionModal.classList.toggle('modal-close');
+  });
+  // Evoluciones
+  const evolutionContainer = cardContainer.querySelector('.evolutions');
+  const pokEvolution = pokemon.evolution;
+  const arrayOne = Object.values(pokEvolution);
+  const keysArrayOne = Object.keys(pokEvolution);
+
+  const prevEvolution = Object.prototype.hasOwnProperty.call(pokEvolution, 'prev-evolution');
+  const nextEvolution = Object.prototype.hasOwnProperty.call(pokEvolution, 'next-evolution');
+
+  const evolutionCard = (nameP, evolution, cStyle) => {
+    let img = 'assets/No_image_available.svg';
+    for (let i = 0; i < data.pokemon.length; i += 1) {
+      if (data.pokemon[i].name === nameP) {
+        img = data.pokemon[i].img;
+      }
+    }
+    const element = `
+    <div class="${cStyle}">
+      <img src="${img}">
+      <p>${evolution}</p>
+      <p>${nameP}</p>
+    </div>`;
+    return element;
+  };
+  // Pokemons con una evolución y una pre evolución
+  if (prevEvolution && nextEvolution) {
+    const prevName = arrayOne[keysArrayOne.indexOf('prev-evolution')][0].name;
+    const nextName = arrayOne[keysArrayOne.indexOf('next-evolution')][0].name;
+    evolutionContainer.innerHTML = evolutionCard(prevName, 'prev-evolution', 'eachContainer');
+    evolutionContainer.innerHTML += evolutionCard(nextName, 'next-evolution', 'eachContainer');
+    // Pokemons con una o varias evoluciones/pre evoluciones
+  } else if (prevEvolution || nextEvolution) {
+    let evolutionType = '';
+
+    if (prevEvolution) {
+      evolutionType = 'prev-evolution';
+    } else { evolutionType = 'next-evolution'; }
+
+    const arrayTwo = arrayOne[keysArrayOne.indexOf(evolutionType)];
+    if (arrayTwo.length === 1) {
+      evolutionContainer.innerHTML = evolutionCard(arrayTwo[0].name, evolutionType, 'eachContainer');
+
+      if (Object.prototype.hasOwnProperty.call(arrayTwo[0], evolutionType)) {
+        const arrayThree = Object.values(arrayTwo[0]);
+        const keysArrayTwo = Object.keys(arrayTwo[0]);
+        const lastArray = arrayThree[keysArrayTwo.indexOf(evolutionType)];
+        evolutionContainer.innerHTML += evolutionCard(lastArray[0].name, evolutionType, 'eachContainer');
+      }
+    } else {
+      // Pokemons con varias evoluciones
+      arrayTwo.forEach((moreEvolutions) => {
+        evolutionContainer.innerHTML += evolutionCard(moreEvolutions.name, evolutionType, 'moreContainer');
+      });
+    }
+  } else {
+    evolutionContainer.innerHTML += `
+    <p>This pokemons doesn't have evolutions</p>
+    `;
+  }
+  sectionModal.appendChild(cardContainer);
+  return sectionModal;
+};
+
+// MODAL ATTACK
+const attackCard = (pokemon) => {
+  const getTypes = (arr) => {
+    let types = '';
+    arr.forEach((resist) => {
+      types += `${resist} `;
+    });
+    return types;
+  };
+  const sectionModal = document.createElement('section');
+  sectionModal.classList.add('modal-container', 'modal-close');
+  sectionModal.classList.toggle('modal-close');
+  // Card
+  const cardContainer = document.createElement('div');
+  cardContainer.innerHTML = `
+    <div class="modal">
+      <p class="close red-bg">x</p>
+      <section class="cards">
+        <section class="pokemon-name2 red-bg">${pokemon.num} - ${pokemon.name}</section>
+        <section class="info-container">
+          <div class="sub-container-img">
+            <div class="pokemon-screen red-b">
+              <img class="screen-img" src="${pokemon.img}">
+            </div>
+          </div>
+          <div class="stats-container">
+            <p class="borderRows subtitle2">Stats</p>
+            <div class= "name-stats">
+              <p>Base attack: ${pokemon.stats['base-attack']}</p>
+              <p>Base defense: ${pokemon.stats['base-defense']}</p>
+              <p>Base stamina: ${pokemon.stats['base-stamina']}</p>
+              <p>Max CP: ${pokemon.stats['max-cp']}</p>
+              <p>Max HP: ${pokemon.stats['max-hp']}</p>
+            </div>
+          </div>
+        </section>
+        <section class="info-container">
+          <div class="column2">
+            <p class="borderRows subtitle2">Resistant</p>
+            <p>${getTypes(pokemon.resistant)}</p>
+          </div>
+          <div class="column2">
+            <p class="borderRows subtitle2">Weakness</p>
+            <p>${getTypes(pokemon.weaknesses)}</p>
+          </div>
+        </section> 
+        <section class="calculate">
+          <p class="subtitle2 attack-title">Quick-Moves</p>
+          <div class="calculateStats">
+            <p class="longElementName textTitle">Name</p>
+            <p class="element textTitle">Type</p>
+            <p class="longElement textTitle">Base damage</p>
+            <p class="element textTitle">Energy</p>
+            <p class="longElement textTitle">Move duration</p>
+            <p class="element textTitle">DPS</p>
+            <p class="element textTitle">EPS</p>
+          </div> 
+          <div class="quick-move"></div>
+          <p class="subtitle2 attack-title">Special-attacks</p>
+          <div class="calculateStats">
+            <p class="longElementName textTitle">Name</p>
+            <p class="element textTitle">Type</p>
+            <p class="longElement textTitle">Base damage</p>
+            <p class="element textTitle">Energy</p>
+            <p class="longElement textTitle">Move duration</p>
+            <p class="element textTitle">DPS</p>
+            <p class="element textTitle">EPS</p>
+          </div>
+          <div class="special-attack"></div>
+        </section>
+      </section>  
+    </div>`;
+
+  const quickMoveContainer = cardContainer.querySelector('.quick-move');
+  const specialAttackContainer = cardContainer.querySelector('.special-attack');
+  // Cálculos
+  pokemon['quick-move'].forEach((quickMove) => {
+    const divElement = document.createElement('div');
+    divElement.innerHTML = `
+    <div class="borderRows">
+      <p class="longElementName borderColumn textTitle">${quickMove.name}</p>
+      <p class="element borderColumn textTitle">${quickMove.type}</p>
+      <p class="longElement borderColumn textContent">${quickMove['base-damage']}</p>
+      <p class="element borderColumn textContent">${quickMove.energy}</p>
+      <p class="longElement borderColumn textContent">${quickMove['move-duration-seg']}</p>
+      <p class="element borderColumn textContent">${dpsCalculate(quickMove, pokemon.type)}</p>
+      <p class="element textContent ">${epsCalculate(quickMove)}</p>
+    </div>`;
+    quickMoveContainer.appendChild(divElement);
+  });
+
+  pokemon['special-attack'].forEach((attack) => {
+    const divElement = document.createElement('div');
+    divElement.innerHTML = `
+    <div class="borderRows"> 
+      <p class="longElementName borderColumn textBreak">${attack.name}</p>
+      <p class="element borderColumn textTitle">${attack.type}</p>
+      <p class="longElement borderColumn textContent">${attack['base-damage']}</p>
+      <p class="element borderColumn textContent">${attack.energy}</p>
+      <p class="longElement borderColumn textContent">${attack['move-duration-seg']}</p>
+      <p class="element borderColumn textContent">${dpsCalculate(attack, pokemon.type)}</p>
+      <p class="element textContent">${epsCalculate(attack)}</p>
+    </div>
+    `;
+    specialAttackContainer.appendChild(divElement);
+  });
+
+  const close = cardContainer.querySelector('.close');
+  close.addEventListener('click', () => {
+    sectionModal.classList.toggle('modal-close');
+  });
+
+  sectionModal.appendChild(cardContainer);
+  return sectionModal;
+};
+
+
+// Creando card de pokemon
+const pokemonCards = (allPokemons) => {
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('cards-distribution');
+
+  for (let i = 0; i < allPokemons.length; i += 1) {
+    const divElement = document.createElement('div');
+    divElement.setAttribute('class', 'pokemon-card');
+
+    divElement.innerHTML = `
+      <p class="pokemon-number">${allPokemons[i].num}</p>
+      <img class="pokemon-image" src="${allPokemons[i].img}">
+      <p class="pokemon-name">${allPokemons[i].name}</p> 
+      <div class="button-container">
+        <button class="button features">Features</button>
+        <button class="button attacks">Attacks</button>
+      </div>`;
+    cardContainer.appendChild(divElement);
+    // Evento del modal features
+    const btnFeatures = divElement.querySelector('.features');
+    btnFeatures.addEventListener('click', (e) => {
+      e.preventDefault();
+      divElement.appendChild(featuresCard(allPokemons[i]));
+    });
+
+    // Evento del modal attack
+    const btnAttack = divElement.querySelector('.attacks');
+    btnAttack.addEventListener('click', (e) => {
+      e.preventDefault();
+      divElement.appendChild(attackCard(allPokemons[i]));
+    });
+  }
+
+  return cardContainer;
+};
+
+// Creando subtítulos
+const createSub = (principal, second) => {
+  const subtitle = document.createElement('div');
+  subtitle.className = 'subtitles margin-bottom';
+  subtitle.innerHTML = `
+  <div class="arrow">
+    <h2>${principal}: ${second}</h2>
+  </div>
+  <div class="line hide"></div>
+  `;
+  return subtitle;
+};
+
+// Obteniendo todos los pokemones y separando por generación
+const allDataByGenerations = () => {
+  sectionContent.innerHTML = '';
+  sectionContent.classList.remove('show');
+
+  // Creando sección Kanto
+  sectionContent.appendChild(createSub('Generation I', 'Kanto'));
+  const dataKanto = document.createElement('div');
+  dataKanto.className = 'cards-distribution';
+  // Creando e insertando cards de pokemones
+  dataKanto.appendChild(pokemonCards(filterByGeneration(data.pokemon, 'kanto')));
+  sectionContent.appendChild(dataKanto);
+
+  // Creando sección Johto
+  sectionContent.appendChild(createSub('Generation II', 'Johto'));
+  const dataJohto = document.createElement('div');
+  dataJohto.className = 'cards-distribution';
+  // Creando e insertando cards de pokemones
+  dataJohto.appendChild(pokemonCards(filterByGeneration(data.pokemon, 'johto')));
+  sectionContent.appendChild(dataJohto);
+};
+
+// El evento que llama a la función que inserta todos los pokemones al iniciar la página
+window.addEventListener('load', () => {
+  allDataByGenerations();
+});
+
+
+// EVENTOS PARA MANIPULAR ARRAYS DE POKEMONES
+
+// SEARCH
+const searchInput = document.querySelector('#filter-search');
+searchInput.addEventListener('input', () => {
+  const inputText = searchInput.value.toLowerCase();
+  sectionContent.innerHTML = '';
+  // Buscando pokemones
+  const result = search(data.pokemon, inputText);
+  if (inputText.length > 0 && result.length > 0) {
+    sectionContent.appendChild(pokemonCards(result));
+  } else if (inputText.length > 0 && result.length === 0) {
+    sectionContent.textContent = 'Pokemon not found :(';
+  } else {
+    allDataByGenerations();
+  }
+});
+
+// SORT
+const selection = document.getElementById('selection');
+selection.addEventListener('change', () => {
+  const chosenOrder = selection.value;
+  sectionContent.innerHTML = '';
+  sectionContent.appendChild(pokemonCards(order(data.pokemon, chosenOrder)));
+  // Mostrando la data completa por generaciones como defecto
+  if (chosenOrder === 'default') {
+    allDataByGenerations();
+  }
+});
+
+// FILTER
 const filterbox = document.getElementsByTagName('aside')[0];
 const main = document.getElementsByTagName('main')[0];
 // Barra de filtros
@@ -29,112 +386,14 @@ btnByType.addEventListener('click', () => {
     subMenu.classList.add('hide-f');
   }
 });
-// Creando card de pokemon
-const pokemonCards = (allPokemons) => {
-  let dataPokemon = '';
-  allPokemons.forEach((eachPokemon) => {
-    const pokemon = `
-    <div class="pokemon-card">
-      <p class="pokemon-number">${eachPokemon.num}</p>
-      <img class="pokemon-image" src="${eachPokemon.img}">
-      <p class="pokemon-name">${eachPokemon.name}</p> 
-      <div class="button-container">
-        <button class ="button" id="features">Features</button>
-        <button class ="button" id="attacks">Attacks</button>
-      </div>
-    </div>`;
-    dataPokemon += pokemon;
-  });
-  return dataPokemon;
-};
-
-// Creando subtítulos
-const createSub = (principal, second) => {
-  const subtitle = document.createElement('div');
-  subtitle.className = 'subtitles margin-bottom';
-  subtitle.innerHTML = `
-  <div class="arrow">
-    <h2>${principal}: ${second}</h2>
-  </div>
-  <div class="line hide"></div>
-  `;
-  return subtitle;
-};
-
-// Obteniendo todos los pokemones y separando por generación
-const allDataByGenerations = () => {
-  sectionContent.innerHTML = '';
-  // borrando el contenedor del filtrado
-  sectionContent.classList.remove('show');
-  // Creando sección Kanto
-  sectionContent.appendChild(createSub('Generation I', 'Kanto'));
-  const dataKanto = document.createElement('div');
-  dataKanto.className = 'cards-distribution';
-  // Creando e insertando cards de pokemones
-  dataKanto.innerHTML += pokemonCards(filterByGeneration(data.pokemon, 'kanto'));
-  sectionContent.appendChild(dataKanto);
-  // Creando sección Johto
-  sectionContent.appendChild(createSub('Generation II', 'Johto'));
-  const dataJohto = document.createElement('div');
-  dataJohto.className = 'cards-distribution';
-  // Creando e insertando cards de pokemones
-  dataJohto.innerHTML += pokemonCards(filterByGeneration(data.pokemon, 'johto'));
-  sectionContent.appendChild(dataJohto);
-};
-
-// El evento que llama a la función que inserta todos los pokemones al iniciar la página
-window.addEventListener('load', () => {
-  allDataByGenerations();
-});
-
-// Guardando input para buscar
-const searchInput = document.querySelector('#filter-search');
-
-// Evento del input que ejecuta la funcion search
-searchInput.addEventListener('input', () => {
-  const inputText = searchInput.value.toLowerCase();
-  // Crear el contenedor con createElement para search
-  const searchBox = document.createElement('div');
-  searchBox.classList.add('distribution-search');
-  sectionContent.innerHTML = '';
-  // Buscando pokemones
-  const result = search(data.pokemon, inputText);
-  if (inputText.length > 0 && result.length > 0) {
-    searchBox.innerHTML += pokemonCards(result);
-    sectionContent.appendChild(searchBox);
-  } else if (inputText.length > 0 && result.length === 0) {
-    searchBox.innerHTML += 'No se ha encontrado el pokemon :(';
-    sectionContent.appendChild(searchBox);
-  } else {
-    allDataByGenerations();
-  }
-});
-// Sort
-const selection = document.getElementById('selection');
-selection.addEventListener('change', () => {
-  const chosenOrder = selection.value;
-  sectionContent.innerHTML = '';
-  const cardsContainer = document.createElement('div');
-  cardsContainer.className = 'cards-distribution';
-  // Llamando a la función para ordenar y crear las cards
-  cardsContainer.innerHTML += pokemonCards(order(data.pokemon, chosenOrder));
-  sectionContent.appendChild(cardsContainer);
-  // Mostrando la data completo por generaciones como defecto
-  if (chosenOrder === 'default') {
-    allDataByGenerations();
-  }
-});
-// Filter
+// Evento filter
 const filterBox = document.querySelector('.sub-menu');
 filterBox.addEventListener('click', (e) => {
   const typeChose = e.target.id;
   sectionContent.innerHTML = '';
   sectionContent.appendChild(createSub('Type', e.target.id));
-  const filterContainer = document.createElement('div');
-  filterContainer.className = 'cards-distribution';
   // Llamar a la función para filtrar por tipo elegido
-  filterContainer.innerHTML += pokemonCards(filterByType(data.pokemon, typeChose));
-  sectionContent.appendChild(filterContainer);
+  sectionContent.appendChild(pokemonCards(filterByType(data.pokemon, typeChose)));
 });
 // All pokémons
 const allPokemons = document.getElementById('all-pokemons');
@@ -142,6 +401,7 @@ allPokemons.addEventListener('click', () => {
   pokemonCards(order(data.pokemon, ''));
   allDataByGenerations();
 });
+
 // HOME
 const home = document.getElementById('home');
 home.addEventListener('click', () => {
@@ -149,7 +409,7 @@ home.addEventListener('click', () => {
   location.reload();
 });
 
-// Botón de subir
+// BOTÓN DE SUBIR
 window.onscroll = () => {
   if (document.documentElement.scrollTop > 100) {
     document.querySelector('.go-top-container').classList.add('show');
